96fd31115363d4206817a84cce76e384b4510476
Drack112 <onepessoa3@gmail.com>
2022-06-05 14:35:00 -0300

    ✨ New Feature: All API and frontend finished

---
+++ invoices-api/src/invoices/invoices.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, ValidationPipe } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+import { InvoicesService } from './invoices.service';
+import { KafkaCreateInvoiceDto } from './dto/create-invoice.dto';
+
+@Controller('credit-cards/:creditCardNumber/invoices')
+export class InvoicesController {
+  constructor(private readonly invoicesService: InvoicesService) {}
+
+  @MessagePattern('payments')
+  create(@Payload(new ValidationPipe()) message: KafkaCreateInvoiceDto) {
+    return this.invoicesService.create(message.value);
+  }
+
+  @Get()
+  findAll(@Param('creditCardNumber') creditCardNumber: string) {
+    return this.invoicesService.findAll(creditCardNumber);
+  }
+}
